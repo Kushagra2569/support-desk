1a4f79d90093fbb2ca764f1282488e8e04d37628
Kushagra Gupta <kushagra.kingg@live.com>
2022-03-13 16:13:47 +0530

    added ticket Functionality

---
+++ frontend/src/pages/NewTicket.jsx
@@ -0,0 +1,69 @@
+import { useState,useEffect } from "react"
+import {useSelector, useDispatch} from "react-redux"
+import {useNavigate} from "react-router-dom"
+import {toast} from "react-toastify"
+import {createTicket, reset} from '../features/tickets/ticketSlice'
+import Spinner from "../components/Spinner"
+
+
+function NewTicket() {
+    const {user} = useSelector(state => state.auth)
+    const {isLoading, isError, isSuccess, message} = useSelector(state => state.ticket)
+
+    const [name] = useState(user.name)
+    const [email] = useState(user.email)
+    const [product, setProduct] = useState('iPhone')
+    const [description, setDescription] = useState('')
+    
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        dispatch(createTicket({product, description}))
+    }
+  return (
+    <>
+        <section className="heading">
+            <h1>Create new Ticket</h1>
+            <p>Please fill out the form below</p>
+        </section>
+
+        <section className="form">
+            <div className="form-group">
+                <label htmlFor="name">Customer Name</label>
+                <input type="text" className="form-control" value={name} disabled/>
+            </div>
+            <div className="form-group">
+                <label htmlFor="email">Customer Email</label>
+                <input type="text" className="form-control" value={email} disabled/>
+            </div>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label htmlFor="product">Product</label>
+                    <select name="product" id="product" value={product} onChange={(e) => {
+                        return setProduct(e.target.value)
+                    }}>
+                        <option value="iPhone">iPhone</option>
+                        <option value="iPad">iPad</option>
+                        <option value="Macbook Pro">Macbook Pro</option>
+                        <option value="iMac">iMac</option>
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="description">Description of the issue</label>
+                    <textarea name="description" id="description" className="form-control" placeholder="Description" value={description} onChange={(e) => {
+                        return setDescription(e.target.value)
+                    }}></textarea>
+                </div>
+                <div className="form-group">
+                    <button className="btn btn-block">Submit</button>
+                </div>
+            </form>
+        </section>
+    </>
+  )
+}
+
+export default NewTicket
